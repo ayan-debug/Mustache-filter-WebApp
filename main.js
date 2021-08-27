4ae60e89cd7d326e24205385e68842d1f9e78b63
ayan-debug <70314546+ayan-debug@users.noreply.github.com>
2021-08-27 15:33:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+noseX = 0;
+noseY = 0;
 
-function preload(){
+function preload()
+{
+    mustache = loadImage('https://i.postimg.cc/J7cVKNx5/mustache-new.png');
 }
 
 function setup()
@@ -24,14 +28,19 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 50;
+        noseY = results[0].pose.nose.y ;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 function draw()
 {
   image(video, 0, 0, 300, 300);
+  fill(255,0,0);
+  stroke(255,0,0);
+  image(mustache, noseX, noseY, 100 , 100);
 }
 
 function take_snapshot()
